124ada7a2d39f59f7673a6a6fe99ec4db022e68f
ModPhoenix <ivanchenko.b@gmail.com>
2021-04-18 23:57:44 +0300

    feat: health_check and logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
 use sqlx::postgres::PgPoolOptions;
 use std::env;
@@ -20,6 +20,10 @@ async fn index() -> impl Responder {
     )
 }
 
+pub async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
@@ -39,7 +43,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
+            .wrap(Logger::default())
             .route("/", web::get().to(index))
+            .route("/health_check", web::get().to(health_check))
             .service(
                 web::scope("/api").service(web::scope("/v1").configure(products::handlers::config)),
             )
